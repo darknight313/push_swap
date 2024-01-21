9305a375747dd29ecd7922f60f9bfb696dc8306f
darknight <129665368+darknight313@users.noreply.github.com>
2024-01-21 16:22:28 +0100

    Update push_swap.c

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -29,6 +29,90 @@ void	sa(s_stack **stack_a)
 	stack_t->next->data = tmp;
 }
 
+void	sb(s_stack **stack_b)
+{
+	s_stack *stack_p = *stack_b;
+	if (count_nodes(&stack_p) < 2)
+		return ;
+	int tmp = stack_p->data;
+	stack_p->data = stack_p->next->data;
+	stack_p->next->data = tmp;
+}
+
+void	ss(s_stack **stack_a, s_stack **stack_b)
+{
+	sa(stack_a);
+	sb(stack_b);
+}
+
+void	pa(s_stack **stack_a, s_stack **stack_b)
+{
+	s_stack *stack_p = *stack_b;
+	if (count_nodes(&stack_p) < 1)
+		return ;
+	int tmp = stack_p->data;
+	append_stack(stack_a, tmp);
+	stack_p = (*stack_b)->next;
+	if (stack_p == NULL)
+	{
+		*stack_b = NULL;
+		return ;
+	}
+	stack_p->pre = NULL;
+	*stack_b = stack_p;
+}
+
+void	pb(s_stack **stack_a, s_stack **stack_b)
+{
+	s_stack *stack_t = *stack_a;
+	if (count_nodes(&stack_t) < 1)
+		return ;
+	int tmp = stack_t->data;
+	append_stack(stack_b, tmp);
+	stack_t = (*stack_a)->next;
+	if (stack_t == NULL)
+	{
+		*stack_a = NULL;
+		return ;
+	}
+	stack_t->pre = NULL;
+	*stack_a = stack_t;
+}
+
+void	ra(s_stack **stack_a)
+{
+	s_stack *stack_t = *stack_a;
+	s_stack *stack_tmp;
+	if (!stack_t)
+		return ;
+	while(stack_t)
+	{
+		append_stack(&stack_tmp, stack_t->data);
+		stack_t = stack_t->next;
+	}
+	*stack_a = stack_tmp;
+}
+
+void	rb(s_stack **stack_b)
+{
+	s_stack *stack_p = *stack_b;
+	s_stack *stack_tmp;
+	if (!stack_p)
+		return ;
+	while(stack_p)
+	{
+		append_stack(&stack_tmp, stack_p->data);
+		stack_p = stack_p->next;
+	}
+	*stack_b = stack_tmp;
+}
+
+void	rr(s_stack **stack_a, s_stack **stack_b)
+{
+	ra(stack_a);
+	rb(stack_b);
+}
+
 int	main(int args, char **argv)
 {
 	s_stack *stack_a = NULL;
